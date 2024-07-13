01bb44da19386ff39e73984c73e7d818ad454fdc
KoTko (Konstantin Bagdasarov) <75670213+ZKoTko@users.noreply.github.com>
2024-07-13 11:42:53 +0400

    main

---
+++ assets/js/sliders.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.restaurant-slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
+
+$(document).ready(function(){
+    $('.bar-slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
+
+$(document).ready(function(){
+    $('.team-slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
