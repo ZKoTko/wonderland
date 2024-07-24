5ca45898f7ba8ff90e9918eecfa2e411dcdc9cfd
KoTko (Konstantin Bagdasarov) <75670213+ZKoTko@users.noreply.github.com>
2024-07-24 17:32:53 +0400

    z

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -24,3 +24,31 @@ $(document).ready(function(){
         slidesToScroll: 1
     });
 });
+
+$(document).ready(function(){
+    $('.choise-items-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '100px', 
+        focusOnSelect: true,
+        infinite: false, 
+        speed: 300,
+        initialSlide: 0,
+        dots: true  
+    });
+});
+
+$(document).ready(function(){
+    $('.choise-items-slider2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true, 
+        centerPadding: '100px', 
+        focusOnSelect: true, 
+        infinite: false, 
+        speed: 300, 
+        initialSlide: 0,
+        dots: true   
+    });
+});
